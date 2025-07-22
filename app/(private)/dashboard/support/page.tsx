212ac34af988ac09deeaa6ad6b624fe53c7ebad9
Dhruv Khade <dhruvkhade05@gmail.com>
2025-07-22 19:02:40 +0530

    changes done

--- app/(private)/dashboard/support/page.tsx
+++ app/(private)/dashboard/support/page.tsx
@@ -63,27 +63,27 @@ const Support = () => {
     let data = [
         {
             title: "What is CROOSE?",
-            start:"A:",
-            description: "Croose is a platform that helps you automate your small business using WhatsApp. You canmanage bookings, payments, customer chats, and even run your inventory — all in one place. It’s like having your own 24/7 assistant on WhatsApp, giving your customers a smooth andmodern experience"
+          
+            description: "Croose is a platform that helps you automate your small business using WhatsApp. You can manage bookings, payments, customer chats, and even run your inventory — all in one place. It’s like having your own 24/7 assistant on WhatsApp, giving your customers a smooth andmodern experience"
         },
         {
             title: "How do I set up the automated response?",
-             start:"A:",
+           
             description: 'Once you complete your onboarding on Croose, your WhatsApp assistant is ready to go. Youcan customize automated responses by adding details about your business in your "Space IQ" and uploading any documents or information you want your assistant to use when talking toyour customers.'
         },
         {
             title: "How do I cancel subscription?",
-             start:"A:",
+             
             description: "You can request cancellation anytime by contacting support through the Croose support bot orreaching us directly via WhatsApp. We’ll process your cancellation before your next billing cycle. Please note that cancellation stops your WhatsApp assistant from operating immediately after the current billing period ends."
         },
         {
             title: "What payment methods do you accept?",
-             start:"A:",
+             
             description: "We accept payments via Stripe for international customers and mobile money for customers in Ghana. If you pay by mobile money, we’ll show you how many days remain in your subscription and remind you 5 days before renewal so you can top up on time."
         },
          {
             title: "How do I set up the automated response?",
-             start:"A:",
+             
             description: "sdfwqdvadcsscdasdcascascds"
         }
 
